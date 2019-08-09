370e18a2d9dbc8dfdec62bb13bdec8db1db24b19
gumarov1991 <gumarov2017@yandex.ru>
2019-08-09 08:12:45 +0600

    fix test

--- tests/fixtures/dataTest.php
+++ tests/fixtures/dataTest.php
@@ -10,5 +10,5 @@ class DataTest
 
     const FILE_YAML_1 = '../tests/fixtures/before.yml';
     const FILE_YAML_2 = '../tests/fixtures/after.yml';
-    const RESULT_YAML = '{ host: hexlet.io, + timeout: 20, - timeout: 50, - proxy: 123.234.53.22, + verbose: true}';
+    const RESULT_YAML = '{"  host":"hexlet.io","+ timeout":20,"- timeout":50,"- proxy":"123.234.53.22","+ verbose":true}';
 }
