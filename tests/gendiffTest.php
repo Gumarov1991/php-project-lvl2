f791a6d5d49614828ba60e4b0204732564e5d6bc
gumarov1991 <gumarov2017@yandex.ru>
2019-08-16 12:13:20 +0600

    fix test

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -6,12 +6,13 @@ use Differ\tests\fixtures;
 use function Differ\genDiff\genDiff;
 use PHPUnit\Framework\TestCase;
 
-const FILE_JSON_1 = '../tests/fixtures/before.json';
-const FILE_JSON_2 = '../tests/fixtures/after.json';
-const FILE_YAML_1 = '../tests/fixtures/before.yml';
-const FILE_YAML_2 = '../tests/fixtures/after.yml';
-const FILE_R_JSON_1 = '../tests/fixtures/recursiveBefore.json';
-const FILE_R_JSON_2 = '../tests/fixtures/recursiveAfter.json';
+const DIR_FIXTURES = '../tests/fixtures/'
+const FILE_JSON_1 = 'before.json';
+const FILE_JSON_2 = 'after.json';
+const FILE_YAML_1 = 'before.yml';
+const FILE_YAML_2 = 'after.yml';
+const FILE_R_JSON_1 = 'recursiveBefore.json';
+const FILE_R_JSON_2 = 'recursiveAfter.json';
 
 const RES_JSON_PRETTY = __DIR__ . '/fixtures/resJsonPretty';
 const RES_YAML_JSON = __DIR__ . '/fixtures/resYamlJson';
@@ -24,9 +25,9 @@ class GenDiffTest extends TestCase
      * @dataProvider additionProvider
     */
 
-    public function testGendiff($expected, $pathToFile1, $pathToFile2, $format = 'pretty')
+    public function testGendiff($expected, $nameFile1, $nameFile2, $format = 'pretty')
     {
-        $this->assertEquals(file_get_contents($expected), genDiff($pathToFile1, $pathToFile2, $format));
+        $this->assertEquals(file_get_contents($expected), genDiff(DIR_FIXTURES . $nameFile1, DIR_FIXTURES . $nameFile1, $format));
     }
 
     public function additionProvider()
