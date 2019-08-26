d071a8a7f43344181a4bfc54e540d49181cc1c74
gumarov1991 <gumarov2017@yandex.ru>
2019-08-26 07:10:57 +0600

    fix parse

--- src/parsers.php
+++ src/parsers.php
@@ -9,22 +9,25 @@ function genData($pathToFile)
 {
     $absolutPath = $pathToFile[0] === '/' ? $pathToFile : __DIR__ . "/{$pathToFile}";
     $extensionFile = pathinfo($absolutPath, PATHINFO_EXTENSION);
-    return parseFile($extensionFile, $absolutPath);
+    if (file_exists($absolutPath)) {
+        $contentFile = file_get_contents($absolutPath);
+        return parse($contentFile, $extensionFile);
+    }
+    throw new \Exception("The '{$pathToFile}' doesn't exists");
 }
 
-function parseFile($extension, $path)
+function parse($data, $extension)
 {
     $mapping = [
-        'yml' => function ($path) {
-            return Yaml::parseFile($path);
+        'yml' => function ($data) {
+            return Yaml::parse($data);
         },
-        'json' => function ($path) {
-            return json_decode(file_get_contents($path), true);
+        'json' => function ($data) {
+            return json_decode($data, true);
         }
     ];
     if (isset($mapping[$extension])) {
-        return $mapping[$extension]($path);
-    } else {
-        throw new \Exception("Error reading file '{$path}'");
+        return $mapping[$extension]($data);
     }
+    throw new \Exception("The '.{$extensionFile}' extension is not supported");
 }
