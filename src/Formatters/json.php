0cb6afca945ca6c274eb63eb339226a626f3c2c6
gumarov1991 <gumarov2017@yandex.ru>
2019-09-08 10:36:15 +0600

    refactor json

--- src/Formatters/json.php
+++ src/Formatters/json.php
@@ -7,31 +7,30 @@ function render($data)
     $arrResult = array_reduce($data, function ($acc, $value) {
         $status = $value['status'];
         $name = $value['name'];
-        $oldValue = json_encode($value['oldValue']);
-        $newValue = json_encode($value['newValue']);
+        $oldValue = $value['oldValue'];
+        $newValue = $value['newValue'];
         $children = $value['children'];
-
+        
         switch ($status) {
             case 'nested':
-                $children = render($value['children']);
-                $acc[] = "\"  $name\"" . ": " . $children;
+                $children = render($children);
+                $acc["  $name"] = $children;
                 break;
             case 'not changed':
-                $acc[] = "\"  $name\"" . ": " . $oldValue;
+                $acc["  $name"] = $oldValue;
                 break;
             case 'deleted':
-                $acc[] = "\"- $name\"" . ": " . $oldValue;
+                $acc["- $name"] = $oldValue;
                 break;
             case 'added':
-                $acc[] = "\"+ $name\"" . ": " . $oldValue;
+                $acc["+ $name"] = $oldValue;
                 break;
             case 'changed':
-                $acc[] = "\"+ $name\"" . ": " . $newValue;
-                $acc[] = "\"- $name\"" . ": " . $oldValue;
+                $acc["+ $name"] = $newValue;
+                $acc["- $name"] = $oldValue;
                 break;
         }
         return $acc;
     }, []);
-    $strResult = "{" . implode(',', $arrResult) . "}";
-    return $strResult;
+    return stripslashes(json_encode($arrResult));
 }
